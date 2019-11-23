7e899b24d81a3fe3a734f2a16694e00c25db5307
shail06 <shailesh.wadhwa@gmail.com>
2019-11-23 17:15:05 +0000

    Amended all resolve functions and Query & mutation to MongoDb works

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _=require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 // Different Types imported fom the graphql package
 const {GraphQLObjectType, 
@@ -9,6 +11,7 @@ const {GraphQLObjectType,
         GraphQLInt,
     GraphQLList} = graphql;
 
+/*
 // Array Containing the dummy data 
 var books = [
     {name: 'Book1', id: '1', genre: 'G1', authorId: '1'},
@@ -23,7 +26,7 @@ var authors = [
     {name: 'Author2', id: '2', age: 25},
     {name: 'Author3', id: '3', age: 12}
 ]
-
+*/
 // Defined a new type which is part of Problem i.e. A book
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -34,7 +37,8 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args){
-                return _.find(authors, {id: parent.authorId});
+                // return _.find(authors, {id: parent.authorId}); // To fetch data from array
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -51,7 +55,8 @@ const AuthorType = new GraphQLObjectType(
                 books: {
                     type: new GraphQLList(BookType),
                     resolve(parent, args){
-                        return _.filter(books, {authorId: parent.id});
+                        // return _.filter(books, {authorId: parent.id});
+                        return Book.find({authorId: parent.id});
                     }
                 }
             }
@@ -69,32 +74,75 @@ const RootQuery = new GraphQLObjectType({
             type: BookType, 
             args:{id:{type: GraphQLID}},
             resolve(parent, args){
-                return _.find(books, {id:args.id});
+                // return _.find(books, {id:args.id});
+                return Book.findById(args.id);
             }
         },
         author:{
             type: AuthorType,
             args:{id:{type: GraphQLID}},
             resolve(parent, args){
-                return _.find(authors, {id: args.id});
+                //return _.find(authors, {id: args.id});
+                return Author.findById(args.id);
             }
         },
         books:{
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                return books
+                return Book.find({});
             }
         },
         authors:{
             type: new GraphQLList(AuthorType),
             resolve(parent, args){
-                return authors
+                return Author.find({}); // Finds all the collections in MongoDb
+            }
+        }
+    }
+});
+/*
+Creating a Mutation. A mutation takes in the given data and stores it into the database
+as compared to a given graph query which just fetches the data.
+*/
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields:{
+        addAuthor:{
+            type: AuthorType,
+            args:{
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent, args){
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        },
+        addBook:{
+            type: BookType,
+            args:{
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
             }
         }
     }
 });
 
+
 // The query exported from here is imported in app.js. This contains the whole schema object
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
